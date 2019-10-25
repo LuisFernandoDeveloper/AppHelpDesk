8cd14129073724ea51516db691c650e8d3df8b11
LuisFernandoDeveloper <luifmoreira@gmail.com>
2019-10-25 20:12:39 -0300

    Segurança no back-end

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -6,7 +6,7 @@
 $chamados = array();
 
 //abrir arquivo para leitura com parametro 'r'
-$arquivo = fopen('arquivo.hd', 'r');
+$arquivo = fopen('../../../app_help_desk/arquivo.hd', 'r');
 
 //enquanto houver registros (linhas) a serem recuperados
 while(!feof($arquivo)){
